f4b6bdf70dfd26074708b9e97897202a8ec4dfdd
ganesh adiga <gani.adiga@gmail.com>
2021-06-24 22:56:22 +0530

    Match APi by method query mapping : needs refactor

--- src/main/java/io/javabrains/ipldashboard/TeamRepository/MatchRepository.java
+++ src/main/java/io/javabrains/ipldashboard/TeamRepository/MatchRepository.java
@@ -1,5 +1,6 @@
 package io.javabrains.ipldashboard.TeamRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -11,8 +12,10 @@ import io.javabrains.ipldashboard.model.Match;
 public interface MatchRepository extends CrudRepository<Match, Long>{
 
 
-    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teanName2,Pageable pageable);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2,Pageable pageable);
 
+    List<Match> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String teamName1, LocalDate date1, LocalDate date2,String teamName2,LocalDate date3, LocalDate date4);
+    
     default List<Match> findLatestMatchesByTeam(String teamName, int count){
        return this.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     }
--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,11 +1,16 @@
 package io.javabrains.ipldashboard.controller;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.javabrains.ipldashboard.TeamRepository.MatchRepository;
 import io.javabrains.ipldashboard.TeamRepository.TeamRepository;
+import io.javabrains.ipldashboard.model.Match;
 import io.javabrains.ipldashboard.model.Team;
 
 @RestController
@@ -25,6 +30,13 @@ public class TeamController {
         Team team = teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year+1, 1, 1);
+        return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, startDate, endDate,teamName, startDate, endDate);
     }
 
 }
