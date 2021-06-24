61689845db76fe18c86a6e2aa5da2e2a9ffdaaf0
ganesh adiga <gani.adiga@gmail.com>
2021-06-24 20:34:13 +0530

    Added React Routers

--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -1,4 +1,5 @@
 package io.javabrains.ipldashboard.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import io.javabrains.ipldashboard.TeamRepository.TeamRepository;
 import io.javabrains.ipldashboard.model.Team;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
