1ec92c98af489ab86dba2d95ac5ce3d91268eaa4
ganesh adiga <gani.adiga@gmail.com>
2021-06-30 14:43:16 +0530

    Cleanup n Deployed the React app in public folder

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,7 +1,5 @@
 import { React, useEffect, useState } from 'react';
-
 import './HomePage.scss';
-import { Link } from 'react-router-dom';
 import { TeamTile } from '../components/TeamTile'
 
 
@@ -10,11 +8,10 @@ export const HomePage = () => {
 
     const [teams, setTeams] = useState([]);
 
-
     useEffect(
         () => {
             const fetchAllTeams = async () => {
-                const response = await fetch(`http://localhost:8080/team/`);
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team/`);
                 const data = await response.json();
                 console.log(data);
                 setTeams(data);
@@ -26,15 +23,14 @@ export const HomePage = () => {
 
     return (
         <div className="HomePage">
+
             <div className="header-section">
                 <h1 className="app-name">IPL Dashboard</h1>
             </div>
 
             <div className="team-grid">
-                {teams.map(team => <TeamTile teamName={team.teamName} />)}
+                {teams.map(team => <TeamTile key={team.teamName} teamName={team.teamName} />)}
             </div>
-
-
         </div>
-    );
+    )
 }
